329f26d7f1000e515674ef53f2c8cb4672b87400
Sasipasch <sasipas.ch@chococrm.com>
2023-03-14 14:46:14 +0700

    config: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,42 +27,7 @@ module.exports = {
     project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project: './tsconfig.json',
-      },
-    },
-    react: {
-      version: '18.x',
-    },
-  },
   rules: {
-    'linebreak-style': 'off',
     'react/react-in-jsx-scope': 0,
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'all',
-        tabWidth: 2,
-        semi: true,
-        singleQuote: true,
-        printWidth: 120,
-        bracketSpacing: true,
-      },
-    ],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          '{}': false,
-        },
-      },
-    ],
-    'react-hooks/exhaustive-deps': 'off',
-    'object-shorthand': 'error',
-    'no-console': 'warn',
   },
 };
