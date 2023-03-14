5c70e406615c7ac7c20b04128952c1f1666c9706
Sasipasch <sasipas.ch@chococrm.com>
2023-03-14 15:45:34 +0700

    chore: require plugin daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 module.exports = {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
